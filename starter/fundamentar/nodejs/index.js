ccfa78d8ef8a65d697edd6d63ed02cd9d44f5eaa
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-04-10 12:49:11 -0300

    setTimeout

--- starter/fundamentar/nodejs/index.js
+++ starter/fundamentar/nodejs/index.js
@@ -1,62 +1,11 @@
-// Array com as perguntas
-const questions = [
-  'O que aprendi hoje?',
-  'O que me deixou aborrecido? E o que eu poderia fazer para melhorar?',
-  'O que me deixou feliz hoje?',
-  'Quantas pessoas ajudei hoje?',
-]
+const timeOut = 3000
+const finished = () => console.log('Done!')
 
-// Função ask que inicia com index de 0, ela retorna
-// o process stdout que vai renderizar as questões no index passado
-// e concatenar com um ">" para mostrar a resposta.
-const ask = (index = 0) => {
-  return process.stdout.write('\n\n' + questions[index] + ' > ')
-}
+// O setTimeout vai guardar a função finished
+// na memória pra ser executada depois de X ms
 
-ask()
-
-// Array de respostas que digitei no CLI.
-const answers = []
-
-// Então, ao passar os dados vou colocar tudo para a array de respostas.
-process.stdin.on('data', (data) => {
-  answers.push(data.toString().trim())
-
-  // Se o tamanho da array de respostas for menor que a de perguntas
-  // então ask() vai ter o index do tamanho da array de respostas
-  // ou seja, a cada resposta que dou, vai aumentando o tamanho
-  // da array e consequentemente passando para a próxima pergunta.
-
-  // Se answers.lenght for 0, então é menor que questions.lenght
-  // então ask vai ter 0 no index e como tem 0 no index
-  // ele vai receber no index de questions, assim, renderizando a pergunta
-  if (answers.length < questions.length) {
-    ask(answers.length)
-  } else {
-    // Quando o tamanho da array de respostas for maior que o de perguntas
-    // a aplicação irá fechar.
-    process.exit()
-  }
-})
-
-process.on('exit', () => {
-  console.log(
-    `
-        Show, João!
-
-        O que você aprendeu hoje foi:
-        ${answers[0]}
-
-        O que te aborreceu e você poderia melhorar foi:
-        ${answers[1]}
-
-        O que te deixou feliz hoje:
-        ${answers[2]}
-
-        Você ajudou ${answers[3]} pessoas hoje!
-
-        Volte amanhã para novas reflexões!
-
-        `
-  )
-})
+// Então pode-se dizer que o setTimeout é assincrono
+// pois, se não fosse, o console.log('Mostrar')
+// teria esperado o timeout acabar pra assim ser executado
+setTimeout(finished, timeOut)
+console.log('Mostrar')
