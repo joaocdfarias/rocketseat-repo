44a76435676c2260fd245735f3a26eefb26c008e
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-04-12 22:35:28 -0300

    EJS ForEach, Variables

--- starter/fundamentar/nodejs-ejs/server.js
+++ starter/fundamentar/nodejs-ejs/server.js
@@ -4,7 +4,20 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('pages/index')
+  const items = [
+    { title: 'D', message: 'esenvolver aplicações/serviços de forma fácil' },
+    { title: 'E', message: 'JS usa JavaScript para rendereizar HTML' },
+    { title: 'M', message: 'uito fácil de usar' },
+    { title: 'A', message: ' escrita é facil' },
+    { title: 'I', message: 'nstall EJS' },
+    { title: 'S', message: 'intaxe simples' },
+  ]
+
+  const subtitle = 'Linguagem de modelagem para criação de HTML'
+  res.render('pages/index', {
+    qualities: items,
+    subtitle: subtitle,
+  })
 })
 
 app.get('/about', function (req, res) {
